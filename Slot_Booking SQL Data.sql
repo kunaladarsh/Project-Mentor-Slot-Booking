eeca55157ce87373ff2c5ea00695866f5af58b68
Adarsh Kunal <adarshkunal5772@gmail.com>
2023-04-22 12:05:22 +0530

    MYSQl

---
+++ Slot_Booking SQL Data.sql
@@ -0,0 +1,14 @@
+create database MentorSlotBooking;
+use MentorSlotBooking;
+create table signup1(name varchar(50), 
+sapid varchar(50), 
+roll varchar(50),
+dept varchar(50),
+proj varchar(50), 
+group1 varchar(50), 
+contact varchar(50), 
+password varchar(50), 
+cpassword varchar(50));
+
+select *from Signup1
+
