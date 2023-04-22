c256181ec016ddb13cca45714ef9523cae0119ed
Adarsh Kunal <adarshkunal5772@gmail.com>
2023-04-22 17:08:51 +0530

    updated Sql

--- Slot_Booking SQL Data.sql
+++ Slot_Booking SQL Data.sql
@@ -5,10 +5,13 @@ sapid varchar(50),
 roll varchar(50),
 dept varchar(50),
 proj varchar(50), 
-group1 varchar(50), 
+semester varchar(50), 
+pyear varchar(50),
+tproject varchar(50),
 contact varchar(50), 
+mentorn varchar(50), 
 password varchar(50), 
 cpassword varchar(50));
 
-select *from Signup1
-
+drop table signup1;
+select *from Signup1 
